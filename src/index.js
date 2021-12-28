10c743e7c272039fa6675433970924ca754a0f23
Quentin Gérôme <geromequentin@gmail.com>
2021-12-28 16:51:26 +0100

    feat(Auth): Implement a change password page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import ProfileMainMenu from "./components/ProfileMainMenu";
-import { ChangePasswordPage } from "./components/ChangePasswordPage";
+import ChangePasswordPage from "./components/ChangePasswordPage";
 import messages_en from "./translations/en.json";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
-  "core.Router": [
-    { path: "profile/changePassword", component: ChangePasswordPage },
-  ],
-  "core.MainMenu": [ProfileMainMenu]
-}
+  "core.Router": [{ path: "profile/changePassword", component: ChangePasswordPage }],
+  "core.MainMenu": [ProfileMainMenu],
+};
 
 export const ProfileModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
-}
+};
