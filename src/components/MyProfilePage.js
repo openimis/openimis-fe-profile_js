020d2aacfd1fa484fc51ce14d828ff8770674683
lruzicki <lukasz.ruz@gmail.com>
2023-03-20 16:47:34 +0100

    OTC-971: added conditional chaining to prevent from fetching values from null variable

--- src/components/MyProfilePage.js
+++ src/components/MyProfilePage.js
@@ -48,13 +48,13 @@ const MyProfilePage = (props) => {
 
   let regions = [];
   let districts = [];
-  const locations = user?.iUser.userdistrictSet;
+  const locations = user?.iUser?.userdistrictSet || [];
 
   if (locations) {
     locations.map((location) => {
-      districts.push(location.location.name);
-      if (!regions.includes(location.location.parent.name)) {
-        regions.push(location.location.parent.name);
+      districts.push(location?.location?.name);
+      if (!regions.includes(location?.location?.parent?.name)) {
+        regions.push(location?.location?.parent?.name);
       }
     });
   }
@@ -166,7 +166,7 @@ const MyProfilePage = (props) => {
                     module="profile"
                     label="language"
                     name="language"
-                    value={user?.iUser.language.name}
+                    value={user?.iUser?.language?.name}
                     variant="outlined"
                     readOnly={true}
                   />
@@ -191,7 +191,7 @@ const MyProfilePage = (props) => {
                   </TableHead>
                   <TableBody>
                     {user &&
-                      user.iUser.roles.map((role) => (
+                      user?.iUser?.roles.map((role) => (
                         <TableRow key={role.name}>
                           <TableCell component="th" scope="row">
                             {role.name}
