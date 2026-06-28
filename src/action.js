6b610dcfb2aef4022055cb5153bddb0054e652a6
sunil <sunilparajuli2002@gmail.com>
2026-06-28 23:48:51 +0545

    fix: update ProfileModule query

--- src/action.js
+++ src/action.js
@@ -5,7 +5,7 @@ const ROLE_FULL_PROJECTION = () => [
   "lastName",
   "email",
   "phone",
-  "iUser{language{name},defaultRowsPerPage,roles{id, name, altLanguage},userdistrictSet{location{name,parent{name}}}}",
+  "iUser{language{name},roles{id, name, altLanguage},userdistrictSet{location{name,parent{name}}}}",
 ];
 
 export function fetchRoles(params) {
